5a9f40b1103a9c4f6c87f5560132e05743d73da1
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-03-01 09:51:18 +0100

    feat: modification d'un tweet

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -1,7 +1,9 @@
-const { createTweet, deleteTweet } = require('../controllers/tweet.controller')
+const { createTweet, deleteTweet, displayTweet, updateTweet } = require('../controllers/tweet.controller')
 const router = require('express').Router()
 
 router.post('/new', createTweet) // creation d'un tweet
 router.get('/delete/:tweetId', deleteTweet);
+router.get('/edit/:tweetId', displayTweet);
+router.post('/edit/:tweetId', updateTweet);
 
 module.exports = router
