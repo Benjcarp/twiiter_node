c1e17ed16a9658fc05b967bd93008d5308e73d44
Benjamin Carpentier <benjamincarpentier01@gmail.com>
2023-03-02 09:33:49 +0100

    empecher la modification d'un tweet par un autre utilisateur

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const {signup, signupForm} = require('../controllers/user.controller.js');
+const { ensureAuthenticated } = require('../config/security.config.js');
+const {signup, signupForm, uploadImage} = require('../controllers/user.controller.js');
 
 router.get('/signup/form', signupForm);
 router.post('/signup', signup);
+router.post('/update/image',uploadImage, ensureAuthenticated);
 
 module.exports = router;
